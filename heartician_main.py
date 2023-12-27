62b9e0912f012dd215f15c59950f4ed534f97bb3
GamerMePro <53392939+rishit836@users.noreply.github.com>
2023-12-27 20:03:52 +0530

    this is literally me
    90% accuracy SVM models

---
+++ heartician_main.py
@@ -0,0 +1,2 @@
+from ubidots import ApiClient
+api = ApiClient(token="4b00-xxxxxxxxxxxxxxxxxxx", base_url="http://yourcompanyname.api.ubidots.com/api/v1.6/")
--- main_api.py
+++ main_api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import numpy as np
 import pickle
 
-model = pickle.load(open('model_lr.sav', 'rb'))
+model = pickle.load(open('model_clf.sav', 'rb'))
 
 app = Flask(__name__)
 
@@ -14,16 +14,23 @@ def hello():
 
 @app.route('/post', methods=["POST"])
 def predict():
+
     input_data = request.get_data(as_text=True)
     input_data = input_data.replace("[", "")
     input_data = input_data.replace("]", "")
     pred_vals = input_data.split(",")
     for i in range(len(pred_vals)):
         pred_vals[i] = int(pred_vals[i])
-    val = [[0, 0, 0, 0, 0, 0, 0, 0], pred_vals]
-    pred = model.predict_proba(val)
-    prob = round(pred[0][1] * 100, 2)
-    return str("Probability of you having a heart disease is " + str(prob) + "%")
+    val = [[0, 0, 0, 0, 0,0, 0, 0, 0], pred_vals]
+    pred = model.predict(val)
+    pred_prob = model.predict_proba(val)
+    print(pred[1])
+    # converting it to yes or no
+    if pred[1]==1:
+        ret_str = "You are prone to have heart disease,"+"the chances of having are "+str(round(pred_prob[1][1]*100,2)) + "%"
+    else:
+        ret_str = "You are not prone to have heart disease,"+"the chances of not having are "+str(round(pred_prob[1][0]*100,2))+ "%"
+    return str(ret_str)
 
 
 if __name__ == '__main__':
